5ce1731e1764e6e8f790c4bfb94665e1ab21779a
g.isachenko <george.isachenko@tatemgames.com>
2020-02-08 18:16:03 +0200

    Add empty file reader, that returns no numbers

--- nice-project/Program.cs
+++ nice-project/Program.cs
@@ -9,6 +9,7 @@ namespace NiceProject
             Console.WriteLine("I'm doing some very important stuff!");
 
             var fileReader = new TextFileReader("../../../input.txt");
+
             
             var stuff = new ImportantStuff();
             var result = stuff.DoWhatNeedsToBeDone(fileReader);
